ccfdae072237a6745b3d6ef85677d0e4377f24d8
JiangHong <767052531@qq.com>
2023-07-26 19:38:53 +0800

    寄存器控制器

---
+++ PythonCode/532Decoder.py
@@ -0,0 +1,12 @@
+# coding = utf-8
+
+import os
+
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, '532Decoder.bin')
+
+with open(filename, 'wb') as file:
+    for var in range(32):
+        value = 1 << var
+        result = value.to_bytes(4, byteorder='little')
+        file.write(result)
